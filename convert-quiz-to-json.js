7ce004bbeec8c3c349e4b089479e78dba11dabf9
secretrobotron <secretrobotron@gmail.com>
2018-09-19 08:30:08 -0400

    quizzes and conversion script

---
+++ convert-quiz-to-json.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+
+const quizFolder = './quizzes/';
+
+function cleanString (input) {
+  return input.replace(/\s\s+/g, ' ')
+              .replace(/\s*\?\s*/g, '?')
+              ;
+}
+
+fs.readdir(quizFolder, (err, files) => {
+  files.forEach(file => {
+    
+    fs.readFile(quizFolder + file, 'utf8', function(err, fileData) {
+      if (file.substr(file.length - 4, 4) !== '.txt') return;
+
+      const lines = fileData.split('\n');
+
+      const title = lines.shift();
+      const questions = [];
+
+      lines.shift();
+
+      while (lines.length > 0) {
+        let question = cleanString(lines.shift());
+
+        let answers = [
+          lines.shift(),
+          lines.shift(),
+          lines.shift()
+        ];
+
+        answers = answers.map(answer => {
+          return cleanString(answer);
+        });
+
+        lines.shift();
+
+        question = question.replace(/\d+\.\s+/, '');
+        question = question.replace('(chagua moja)', '');
+        question = question.replace('(chagua jibu moja sahihi)', '');
+
+        let entry = {
+          question: question,
+          answers: answers,
+          'answer-type': 'multiple-choice',
+        };
+
+        if (questions.length > 0) {
+          entry['correct-answer'] = 0;
+        }
+
+        questions.push(entry);
+      }
+
+      const outputFilename = quizFolder + file + '.json';
+
+      const quiz = {
+        id: 0,
+        title: title,
+        color: '#45B39D',
+        video: '',
+        questions: questions
+      };
+
+      fs.writeFile(outputFilename, JSON.stringify(quiz, null, 2), 'utf8', err => {
+        console.log('Wrote ' + outputFilename);
+      });
+
+    });
+
+  });
+});
